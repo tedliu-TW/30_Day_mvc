6ee447e8e9afdcca8a006cd5768c6f24ed3a3920
tedliu-TW <m0461006@g2.usc.edu.tw>
2020-12-01 14:02:37 +0800

    新增         //建立與資料庫的連線字串         private readonly static string cnstr = ConfigurationManager.ConnectionStrings["MVC"].ConnectionString;         //建立與資料的連線         private readonly SqlConnection conn = new SqlConnection(cnstr);

--- web1201/Service/GuestbooksDBService.cs
+++ web1201/Service/GuestbooksDBService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,12 @@ namespace web1201.Service
 {
     public class GuestbooksDBService
     {
+        //建立與資料庫的連線字串
+        private readonly static string cnstr = ConfigurationManager.ConnectionStrings["MVC"].ConnectionString;
+        //建立與資料的連線
+        private readonly SqlConnection conn = new SqlConnection(cnstr);
+
+
+
     }
 }

266a55ffd6b240880a3539eef3f61d7e0c8729fb
tedliu-TW <m0461006@g2.usc.edu.tw>
2020-12-01 14:43:08 +0800

    新增留言function

---
+++ web1201/Controllers/GuestbooksController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using web1201.Models;
+using web1201.Service;
+using web1201.ViewModels;
+
+namespace web1201.Controllers
+{
+    public class GuestbooksController : Controller
+    {
+        //宣告Guestbooks 資料表的Service物件
+        private readonly GuestbooksDBService GuestbookService = new GuestbooksDBService();
+        // GET: Guestbooks
+        public ActionResult Index()
+        {
+            //宣告一個新頁面的模型
+            GuestbooksViewModel Data = new GuestbooksViewModel();
+            //Service 中 取得葉面所需陣列資料;
+            //將頁面
+            Data.DataList = GuestbookService.GetDataList();
+
+            return View(Data);
+        }
+        //新增留言
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="Name,Content")]Guestbooks Data)
+        {
+            //使用service來新增資料
+            GuestbookService.InsertGuestbooks(Data);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
--- web1201/Service/GuestbooksDBService.cs
+++ web1201/Service/GuestbooksDBService.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using web1201.Models;
 
 namespace web1201.Service
 {
@@ -14,7 +15,73 @@ namespace web1201.Service
         //建立與資料的連線
         private readonly SqlConnection conn = new SqlConnection(cnstr);
 
+        //取得鄭烈資料方式
+        public List<Guestbooks> GetDataList()
+        {
+            List<Guestbooks> DataList = new List<Guestbooks>();
+            //sql語法
+            string sql = @"select * from Guestbooks";
+            try
+            {
+                //開啟資料庫連線
+                conn.Open();
+                //執行sql指令
+                SqlCommand cmd = new SqlCommand(sql,conn);
+                //取得sql資料
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Guestbooks Data = new Guestbooks();
+                    Data.Id = Convert.ToInt32(dr["Id"]);
+                    Data.Name = dr["Name"].ToString();
+                    Data.Content = dr["Content"].ToString();
+                    Data.CreateTime = Convert.ToDateTime(dr["CreateTime"]);
+                    //確定此則留言是否回覆且不允許空白
+                    if (!dr["ReplyTime"].Equals(DBNull.Value))
+                    {
+                        Data.Reply = dr["Reply"].ToString();
+                        Data.ReplyTime =Convert.ToDateTime(dr["ReplyTime"]);
+                    }
+                    DataList.Add(Data);
+                }
 
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+         return DataList;
+        }
+
+
+
+        //新增資料方式
+        public void InsertGuestbooks(Guestbooks newData)
+        {
+            //sql新增語法
+            string sql = $@"Insert INTO Guestbooks(Name,Content,CreateTime) Values('{newData.Name}','{newData.Content}','{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}')";
+            try
+            {
+                //開啟資料庫連線
+                conn.Open();
+                //執行sql指令
+                SqlCommand cmd = new SqlCommand(sql,conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                //關閉資料庫連線
+                conn.Close();
+            }
+        }
 
     }
 }
